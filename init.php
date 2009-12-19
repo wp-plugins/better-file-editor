f1fcdce0529d57d0804df943990c32778f902832
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-19 05:02:25 +0000

    1.0.1 - Included Bespin Embedded 0.5.2
    git-svn-id: https://plugins.svn.wordpress.org/better-file-editor/trunk@184661 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Better File Editor
 Description: Edit themes and plugins using Mozilla's Bespin code editor.
-Version: 1.0.0
+Version: 1.0.1
 Author: Matt Gibbs
 Author URI: http://pods.uproot.us/
 
@@ -22,6 +22,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+define('BFE_URL', WP_PLUGIN_URL . '/better-file-editor');
+
 function bfe_menu()
 {
     add_options_page('Better File Editor', 'Better File Editor', 0, 'better-file-editor', 'bfe_edit_area');
@@ -29,32 +31,26 @@ function bfe_menu()
 
 function bfe_edit_area()
 {
-    $bfe_url = WP_PLUGIN_URL . '/better-file-editor';
     $upload_dir = wp_upload_dir();
     $upload_dir = str_replace(get_option('siteurl'), '', $upload_dir['baseurl']);
     $upload_dir = str_replace('/uploads', '', $upload_dir);
 ?>
 
-<link rel="stylesheet" type="text/css" href="<?php echo $bfe_url; ?>/style.css" />
-<script type="text/javascript" src="<?php echo $bfe_url; ?>/js/dojo.js"></script>
-<script type="text/javascript" src="<?php echo $bfe_url; ?>/js/jqFileTree.js"></script>
-<script type="text/javascript" src="https://bespin.mozilla.com/embed.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo BFE_URL; ?>/style.css" />
+<script type="text/javascript" src="<?php echo BFE_URL; ?>/js/jqFileTree.js"></script>
+<script type="text/javascript" src="<?php echo BFE_URL; ?>/js/BespinEmbedded.js"></script>
 <script type="text/javascript">
 var editor_file;
-var _editorComponent;
+var auth = '<?php echo md5(AUTH_KEY); ?>';
 
-// Loads and configures the objects that the editor needs
-dojo.addOnLoad(function() {
-    _editorComponent = new bespin.editor.Component('editor', {
-        language: "php",
-        loadfromdiv: false
-    });
-});
+window.onBespinLoad = function() {
+    bespin = document.getElementById("editor").bespin;
+};
 
 jQuery(function() {
     jQuery(".filebox").fileTree({
         root: "<?php echo $upload_dir; ?>/",
-        script: "<?php echo $bfe_url; ?>/filetree.php",
+        script: "<?php echo BFE_URL; ?>/filetree.php",
         multiFolder: false
     },
     function(file) {
@@ -64,11 +60,9 @@ jQuery(function() {
 });
 
 function loadContent() {
-    var auth = '<?php echo md5(AUTH_KEY); ?>';
-
     jQuery.ajax({
         type: "post",
-        url: "<?php echo $bfe_url; ?>/ajax.php",
+        url: "<?php echo BFE_URL; ?>/ajax.php",
         data: "auth="+auth+"&action=load&file="+editor_file,
         success: function(msg) {
             if ("Error" == msg.substr(0, 5)) {
@@ -76,19 +70,18 @@ function loadContent() {
             }
             else {
                 var code = msg;
-                _editorComponent.setContent(code);
+                bespin.setContent(code);
             }
         }
     });
 }
 
 function saveContent() {
-    var auth = '<?php echo md5(AUTH_KEY); ?>';
-    var code = _editorComponent.getContent();
+    var code = bespin.getContent();
 
     jQuery.ajax({
         type: "post",
-        url: "<?php echo $bfe_url; ?>/ajax.php",
+        url: "<?php echo BFE_URL; ?>/ajax.php",
         data: "auth="+auth+"&action=save&file="+editor_file+"&code="+encodeURIComponent(code),
         success: function(msg) {
             if ("Error" == msg.substr(0, 5)) {
@@ -108,13 +101,11 @@ function saveContent() {
     <table style="width:100%" cellpadding="0" cellspacing="0">
         <tr>
             <td style="width:200px; vertical-align:top">
-                <!-- AJAX file browser -->
                 <h3>Select a file or folder</h3>
                 <div class="filebox" style="overflow:auto"></div>
             </td>
             <td style="vertical-align:top">
-                <!-- Edit area -->
-                <div id="editor" style="height:360px"></div>
+                <div id="editor" class="bespin" style="height:360px"></div>
                 <input type="button" class="button" onclick="saveContent()" value="Save File" />
             </td>
         </tr>
