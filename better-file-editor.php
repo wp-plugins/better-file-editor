45e15c67fa456cd80d53e664bbf62bcea15a3075
bpetty <bpetty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-20 08:23:47 +0000

    Added ACE javascript library, and hooked scripts into the appropriate pages.
    git-svn-id: https://plugins.svn.wordpress.org/better-file-editor/branches/development@575032 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- better-file-editor.php
+++ better-file-editor.php
@@ -12,6 +12,20 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 class BetterFileEditorPlugin {
 
 	function BetterFileEditorPlugin() {
+		add_action('admin_footer-theme-editor.php', array($this, 'print_scripts'));
+		add_action('admin_footer-plugin-editor.php', array($this, 'print_scripts'));
+	}
+
+	function print_scripts() {
+		// data-ace-base="js/lib/ace"
+		?>
+			<script src="<?php echo plugins_url( 'js/ace/ace.js' , __FILE__ ); ?>"
+				type="text/javascript" charset="utf-8"></script>
+			<script src="<?php echo plugins_url( 'js/wp-ace.js' , __FILE__ ); ?>"></script>
+			<script type="text/javascript" charset="utf-8">
+				require("wp-ace");
+			</script>
+		<?php
 	}
 
 }
